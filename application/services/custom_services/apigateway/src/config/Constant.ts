7f2146f4dde94e5d43ad419a38fe9fd0830a8b0d
manavalan619 <manavalanr619@gmail.com>
2020-01-22 18:51:58 +0530

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://justfor-app.justfor.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://justfor-app.justfor.svc.cluster.local:3009';
+export const ADMINURL = 'http://justfor-app.justfor.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://justfor-app.justfor.svc.cluster.local:3008';
